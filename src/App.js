5e2bfe0f8461b763a97108bb9b9cf0c1ccd46eeb
Adrian Szlegel <adrian.szlegel@hotmail.com>
2022-09-07 00:41:53 +0200

    added three columns for displaying new data & visual improvements & new api calls

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import Tooltip from '@mui/material/Tooltip';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 import "./App.css";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      black: {
+        main: '#000',
+        contrastText: '#fff',
+      }
+    }
+  });
 
   const [userName, setUserName] = useState('unknown');
+  const [activeHabits, setActiveHabits] = useState(undefined);
+  const [succeededHabits, setSucceededHabits] = useState(undefined);
+  const [failedHabits, setFailedHabits] = useState(undefined);
 
-  axios.get('http://localhost:3001/userName')
+  useEffect(() => {
+    axios.get('http://localhost:3001/userName')
     .then((response) => {
       console.log(response.data.name);
       setUserName(response.data.name);
@@ -22,25 +43,115 @@ function App() {
       console.log('ERROR:', error);
     })
 
+    axios.get('http://localhost:3001/activeHabits')
+    .then((response) => {
+      console.log(response.data);
+      setActiveHabits(response.data);
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
+    })
+
+    axios.get('http://localhost:3001/succeededHabits')
+    .then((response) => {
+      console.log(response.data);
+      setSucceededHabits(response.data);
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
+    })
+
+    axios.get('http://localhost:3001/failedHabits')
+    .then((response) => {
+      console.log(response.data);
+      setFailedHabits(response.data);
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
+    })
+  }, []);
+  
   return (
     <div>
-      {/* header */}
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            { userName === 'unknown' ?  null : userName + "'s" } Habits
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      {/* header */}
+      <ThemeProvider theme={theme}>
+        {/* header */}
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" component="div">
+              { userName === 'unknown' ?  null : userName + "'s" } Habits
+            </Typography>
+            <FormControlLabel control={<Switch />} label="Dark mode" color="black" />
+          </Toolbar>
+        </AppBar>
+        {/* header */}
+        {/* main view */}
+        <main>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <Paper>
+                  <Typography level="h4">Your current habits</Typography>
+                  <ul>
+                    {activeHabits ? 
+                      activeHabits.map((habit) => {
+                        return <li key={`li-item-${habit.title}`}>{habit.title} <i>{habit.excerpt}</i></li>
+                      })
+                    : null}
+                  </ul>
+                  <div style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
+                    <Tooltip title="Add new habit you wish to start working on">
+                      <Button variant="contained">Add new</Button>
+                    </Tooltip>
+                  </div>                  
+                </Paper>
+              </Grid>
+              <Grid item xs={4}>
+                <Paper>
+                  <Typography level="h4">Your past succeed habits</Typography>
+                  <ul>
+                    {succeededHabits ? 
+                      succeededHabits.map((habit) => {
+                        return <li key={`li-item-succeeded-${habit.title}`}>{habit.title} <i>{habit.excerpt}</i></li>
+                      })
+                    : null}
+                  </ul>
+                  <div style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
+                    <Tooltip title="Manually add a habit, which you succeeded in the past">
+                      <Button variant="contained">Add new</Button>
+                    </Tooltip>
+                  </div> 
+                </Paper>
+              </Grid>
+              <Grid item xs={4}>
+                <Paper>
+                  <Typography level="h4">Your past failed habits</Typography>
+                  <ul>
+                    {failedHabits ? 
+                        failedHabits.map((habit) => {
+                          return <li key={`li-item-failed-${habit.title}`}>{habit.title} <i>{habit.excerpt}</i></li>
+                        })
+                    : null}
+                  </ul>
+                  <div style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
+                    <Tooltip title="Manually add a habit, which you failed in the past">
+                      <Button variant="contained">Add new</Button>
+                    </Tooltip>
+                  </div> 
+                </Paper>
+              </Grid>
+            </Grid>
+          </Box>
+        </main>
+        {/* main view */}
+      </ThemeProvider>
     </div>
   );
 }
